1ea43a77d0d286b41b48b0e69382f60ca7b93316
Renan Paixão <renanlemos21@gmail.com>
2022-01-03 00:44:41 -0300

    feat: initial configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }], // Require space after brace
+    'comma-dangle': ['error', 'never'], // Remove comma on the end of objects
+    'import/no-cycle': ['error', { amd: true, commonjs: true }], // Prevents dependencies cycles
+    'import/no-duplicates': ['error'], // Prevent modules duplication
+    quotes: ['error', 'single', { allowTemplateLiterals: true }], // Force single quotes
+    'no-unused-vars': ['error', { args: 'all' }], /* Prevents unused vars. It can ignore vars with a definite
+    template string in your start, if it be pass like a option */
+    'no-unreachable': 'error' // prevents code bellow return, break and continue... statements
+
+  }
 };
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header class="primary" msg="Requisições Http no vue " />
+  <Task-manager class="primary"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
+import TaskManager from './components/TaskManager.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-  },
+    Header,
+    TaskManager
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.primary {
+  background-color: #c8b8db;
 }
 </style>
