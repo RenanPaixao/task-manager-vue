859f6815c6c5a512acd5d4745e1d968186812ac1
Renan Paixão <renanlemos21@gmail.com>
2022-01-10 23:04:11 -0300

    feat: add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+    '@vue/typescript'
+  ],
+
   parserOptions: {
     ecmaVersion: 2020,
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
